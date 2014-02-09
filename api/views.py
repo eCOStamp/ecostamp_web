a31c7e015b64d7eddf9cd917089321ba036e54a8
Pongsakorn U-chupala <knightbaron@gmail.com>
2014-02-09 21:19:31 +0900

    Implemented dummy api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import simplejson as json
 
-# Create your views here.
+
+def dummy(request):
+    try:
+        response = json.loads(request.body)
+        response["success"] = True
+    except json.JSONDecodeError as e:
+        response = {"success": False, "reason": str(e)}
+    return HttpResponse(json.dumps(response))
