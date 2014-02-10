68f2838fe0bdfdaf93bb52948b8bc56bb8c917e0
Pongsakorn U-chupala <knightbaron@gmail.com>
2014-02-10 16:24:07 +0900

    Add uWSGI deployment script

--- ecostamp_web/settings.py
+++ ecostamp_web/settings.py
@@ -88,6 +88,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/root/ecostamp/static"
 
 try:
     from local_settings import *
---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import local, cd
+from time import sleep
+
+WORKING_DIRECTORY = "/root/ecostamp/ecostamp_web"
+INI_FILE = ""
+PID_FILE = "/tmp/ecostamp.pid"
+
+def start():
+	with cd(WORKING_DIRECTORY):
+		local("uwsgi --ini uwsgi.ini")
+
+def stop():
+    local("uwsgi --stop %s" % PID_FILE)
+
+def restart():
+	stop()
+	sleep(1)
+	start()
+
+def status():
+	local("cat %s" % PID_FILE)
+
