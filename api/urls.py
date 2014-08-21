40401315c4892fc72319de0ae6a160c22bb9b2be
Pongsakorn U-chupala <knightbaron@gmail.com>
2014-08-21 16:31:51 +0900

    API for retrieving stamp

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from api import views
 
 urlpatterns = patterns('',
     url(r'^dummy/$', views.dummy, name="dummy"),
+    url(r'^stamp/(?P<key>\w+)/$', views.stamp, name="stamp"),
 )
-
--- api/views.py
+++ api/views.py
@@ -1,19 +1,44 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-from django.utils import simplejson as json
+from core.models import Stamp
+import json
 
 
 def dummy(request):
     try:
-        response = json.loads(request.body)
+        response = json.loads(request.body)  # Only available in POST request
         response["success"] = True
         response["stamp_url"] = "http://ecostamp.naist.jp/static/img/stamp.jpg"
         response["name"] = "Matsumoto Castle"
         response["description"] = "Matsumoto castle is located somewhere in Japan..."
         response["url"] = "http://www.japanese-castle-explorer.com/castle_profile.html?name=Matsumoto"
-    except json.JSONDecodeError as e:
+    except ValueError as e:  # Bad json
         response = {"success": False, "reason": str(e)}
     except TypeError as e:
         response = {"success": False, "reason": str(e)}
     return HttpResponse(json.dumps(response))
 
+
+def stamp(request, key):
+    try:
+        print key
+        stamp = Stamp.objects.get(key=key)
+        response = dict()
+        response["success"] = True
+        response["image_url"] = stamp.image_url
+        response["name"] = stamp.name
+        response["short_description"] = stamp.short_description
+        response["description"] = stamp.description
+        response["url"] = stamp.url
+    except Stamp.DoesNotExist as e:
+        response = {"success": False, "reason": str(e)}
+    except ValueError as e:  # Bad json
+        response = {"success": False, "reason": str(e)}
+    # except TypeError as e:
+    #     response = {"success": False, "reason": str(e)}
+    return HttpResponse(json.dumps(response))
+
+
+def register(request):
+    # TODO: Write me
+    pass
